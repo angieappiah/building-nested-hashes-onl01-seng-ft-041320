181a1c714911ec6d24487a153d335b73009b4275
Angela Appiah <alegnalawn2@yahoo.com>
2020-04-20 17:25:40 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -1,7 +1,7 @@
 def third_challenge
   epic_tragedy = {
    :montague => {
-      :patriarch => {
+      :patriarch => { :name => "Lord_Montague", :age => "53"
         #your key/value pairs here
         },
       :matriarch => {
